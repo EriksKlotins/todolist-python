cbd48683b11bffce895d903bc91c61a1b2028f0d
Julian Frattini <julian.frattini@alice-dsl.net>
2021-08-02 10:19:47 +0200

    Added some documentation to test methods

--- helper.py
+++ helper.py
@@ -6,21 +6,26 @@ NOTSTARTED = 'Not Started'
 INPROGRESS = 'In Progress'
 COMPLETED = 'Completed'
 
+
 def add_to_list(item):
     if len(item) == 0:
-        print ('Empty items not allowed')
-        return None;
+        print('Empty items not allowed')
+        return None
     try:
         conn = sqlite3.connect(DB_PATH)
         c = conn.cursor()
-        c.execute('insert into items(item, status) values(?,?)', (item, NOTSTARTED))
+        c.execute('insert into items(item, status) values(?,?)',
+                  (item, NOTSTARTED))
         conn.commit()
         return {"item": item, "status": NOTSTARTED}
     except Exception as e:
         print('Error: ', e)
         return None
 
+
 todo_list = {}
+
+
 def get_random_task():
     items = get_all_items()
     if len(items) > 0:
@@ -29,11 +34,9 @@ def get_random_task():
         return random.choice(items)
 
 
-def get_item_range(first = 0, last = -1):
+def get_item_range(first=0, last=-1):
     items = get_all_items()
-    return [items[i] if i=0 else items[0] for i in range(first, first + last - 1)]
-
-
+    return [items[i] if i = 0 else items[0] for i in range(first, first + last - 1)]
 
 
 def get_all_items():
@@ -42,10 +45,11 @@ def get_all_items():
         c = conn.cursor()
         c.execute('select * from items')
         rows = c.fetchall()
-        return { "count": len(rows), "items": rows }
+        return {"count": len(rows), "items": rows}
     except Exception as e:
- 
+
         return None
+
 
 def get_item(item):
     try:
@@ -58,9 +62,10 @@ def get_item(item):
     except Exception as e:
         print('Error: ', e)
         return None
-    
+
+
 def update_status(item, status):
-    #Check if the passed status is a valid value
+    # Check if the passed status is a valid value
     if(status.lower().strip() == 'not started'):
         status = NOTSTARTED
     elif(status.lower().strip() == 'in progress'):
@@ -70,7 +75,7 @@ def update_status(item, status):
     else:
         print("Invalid Status - " + status)
         return None
-    
+
     try:
         conn = sqlite3.connect(DB_PATH)
         c = conn.cursor()
@@ -80,6 +85,7 @@ def update_status(item, status):
     except Exception as e:
         print('Error: ', e)
         return None
+
 
 def delete_item(item):
     try:
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -7,6 +7,7 @@ from unittest.mock import patch
 
 class TestHelper:
 
+	# test adding an item to the todo list and asserting that the item has been added with status NOTSTARTED
 	def test_add_to_list_ok(self):
 		with patch('helper.sqlite3') as mock:
 			mock.connect().commit().return_value = None
@@ -14,6 +15,7 @@ class TestHelper:
 			assert item['item'] == 'test item'
 			assert item['status'] == helper.NOTSTARTED
 
+	# test the system returning all existing items in the todo list
 	def test_get_all_items(self):
 		test_data = ['first item','second item', 3]
 		with patch('helper.get_all_items') as mock:
@@ -21,6 +23,7 @@ class TestHelper:
 			items = helper.get_all_items()
 			assert items == test_data
 
+	# same as the previous test, but this time with an empty list
 	def test_get_all_items1(self):
 		test_data = []
 		with patch('helper.get_all_items') as mock:
@@ -28,11 +31,9 @@ class TestHelper:
 			items = helper.get_all_items()
 			assert items == test_data
 
-
+	# test getting items from a specific range of indices, test for multiple indices
 	@patch('helper.get_all_items')	
 	def test_get_item_range(self, fake_items):
-
-
 		fake_items.return_value = []
 		for i in range(-300, 100):
 			fake_items.append([f'item {i}',helper.INPROGRESS])
@@ -43,34 +44,35 @@ class TestHelper:
 		for td in test_data:
 			assert helper.get_item_range(td[1],td[0]) == real_items[td[0]:td[1]]
 
-
-
+	# test that the item status is updated accordingly
 	def test_started(self):
 		with patch('helper.sqlite3') as mock:
 			result = helper.update_status('test item',helper.INPROGRESS)
 			assert result == {'test item':'In Progress'}
 
+# test getting a random task from the list of tasks
 @patch('helper.get_all_items')
 def test_get_random_task(fake_method):
-	fake_method.return_value = ['SOme task']
+	fake_method.return_value = ['Some task']
 	task = helper.get_random_task()
 	assert task == 'No tasks left'
 
 class TestHelper2:
-	
+	# test receiving an item
 	@patch('builtins.print')
 	def test_Getitem(self, fake_print):
 		item = helper.get_item('test item')
 		assert item == helper.INPROGRESS
-		fake_print.assert_called_with('test item');
+		fake_print.assert_called_with('test item')
 
+	# test deleting an item
 	@patch('helper.sqlite3')	
 	def test_del(self, fakedb):
 		aa = helper.delete_item('test item')
 		assert aa == {'item':'test item'}
 
 
-
+# test the complete functionality of the helper class
 @patch('helper.sqlite3')
 def test_all(fakedb):
 	items = ['first','second','last']
